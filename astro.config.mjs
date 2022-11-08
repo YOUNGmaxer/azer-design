500b6c77daa9279416fee74dbb06917db6d85e15
azeryang <517538470@qq.com>
2022-11-08 19:27:08 +0800

    fix: ignore lit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import lit from "@astrojs/lit";
+// import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [lit()],
+  // integrations: [lit()],
   vite: {
     optimizeDeps: {
       include: ['node-domexception/index.js']
